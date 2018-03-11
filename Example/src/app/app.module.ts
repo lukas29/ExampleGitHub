823029fde947b1537df1553f1aa444d08de2518a
Samuel.Arreola <Samuel.Arreola@RepoBox.com.mx>
2018-03-11 01:17:31 -0700

    Cree componente Products

--- Example/src/app/app.module.ts
+++ Example/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RolesComponent } from './roles/roles.component';
 import { ClientsComponent } from './clients/clients.component';
+import { ProductsComponent } from './products/products.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     RolesComponent,
-    ClientsComponent
+    ClientsComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
